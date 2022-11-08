016d54089f5aa937bb27b72c4bcf116623ac31ff
Trome29 <108098922+Trome29@users.noreply.github.com>
2022-11-08 17:17:12 +0200

    create tableVC

---
+++ FirstParsingHW/View Controllers/AboutJokeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AboutJokeViewController.swift
+//  FirstParsingHW
+//
+//  Created by Roman on 08.11.2022.
+//
+
+import UIKit
+
+class AboutJokeViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
